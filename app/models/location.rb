95489a43ea5a57911ac502dc90d1779bc38921e5
Sia <siakaramalegos@gmail.com>
2015-06-04 09:07:10 -0500

    Created Location scaffold and set up model

---
+++ app/models/location.rb
@@ -0,0 +1,8 @@
+class Location < ActiveRecord::Base
+  geocoded_by :my_location   # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
+
+  def my_location
+    "#{address}, #{city}, GA"
+  end
+end
