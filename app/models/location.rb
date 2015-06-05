3ca1e7038f08c726431a93d936cfda5e9a78c82e
Sia <siakaramalegos@gmail.com>
2015-06-05 14:29:26 -0500

    Updated styling

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,11 @@ class Location < ActiveRecord::Base
   geocoded_by :my_location   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
 
+  CITIES = ["Atlanta", "Alpharetta", "Avondale Estates", "Chamblee", "Clarkston",
+    "College Park", "Decatur", "Doraville", "Dunwoody", "East Point",
+    "Fairburn", "Forest Park", "Hapeville", "Lithonia", "Morrow", "Palmetto",
+    "Riverdale", "Roswell"," Sandy Springs", "Stone Mountain", "Union City"]
+
   def my_location
     "#{address}, #{city}, GA"
   end
