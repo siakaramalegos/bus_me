49bf057cd6f2e8204e99e684b88a2e826c5d6b18
Sia <siakaramalegos@gmail.com>
2015-06-14 10:11:48 -0500

    Added validation tests

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -4,4 +4,14 @@ class LocationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save location without address" do
+    location = Location.new
+    assert_not location.save, "Saved the location without an address"
+  end
+
+  test "should not save location without city" do
+    location = Location.new
+    assert_not location.save, "Saved the location without a city"
+  end
 end
